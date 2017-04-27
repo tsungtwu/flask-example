3030d8871a3cd6fe4c324655822e72619ae29a68
tsungwu <jtrmn.wu@gmail.com>
2017-04-27 16:18:45 +0800

    [v2.1.3] add ESDAO module

--- webapp/app/api/__init__.py
+++ webapp/app/api/__init__.py
@@ -3,9 +3,10 @@ from flask_restplus import Api, Namespace
 from app.api.cve.apiController import api as ns1
 from app.api.user.apiController import api as ns2
 from app.api.oauth.apiController import api as ns3
+from app.api.es.apiController import api as ns4
 
 
-api = Api(version='2.1', \
+api = Api(version='2.1.3', \
             title='Flask Restful plus Api', \
             doc='/api', \
             description='Document for Restful api', \
@@ -16,3 +17,4 @@ api = Api(version='2.1', \
 api.add_namespace(ns1, path='/api/cves')
 api.add_namespace(ns2, path='/api/users')
 api.add_namespace(ns3, path='/api/oauth')
+api.add_namespace(ns4, path='/api/es')
